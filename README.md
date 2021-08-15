ecd972662f1a3588a0dd3c0f78146e8a25856fa5
arberx <14798762+arberx@users.noreply.github.com>
2021-08-15 18:59:17 -0400

    Add fully featured tool with command-line and README

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-### Streeteasy grep
+## streeteasy-grep
+
+Query [streeteasy.com](https://www.streeteasy.com) rentals and get the results in json format. Tool also always you to diff between diffferent innvocations of the same query.
+
+### Motivation
+
+`streeteasy` currently limits bots and scrapers from accessing the site programmitically with rate-limiting and captcha features.
+
+Using [selenium](https://selenium-python.readthedocs.io/), we're able to replicate non-bot behavior and therefore allowing us to parse the site.
+
+### Installation
+
+First install the Firefox Gecko drivers locally for Firefox (the directions can be found [here](https://selenium-python.readthedocs.io/)), and have the Firefox browser installed. Any browser can be used, but requires changing the driver setup in `parser.py`.
+
+It currently relies on [poetry](https://python-poetry.org/) for installation and dependency management. Follow the `poetry` docs for installation.
+
+Once `poetry` is installed run:
+
+```
+poetry install
+```
+
+and
+
+```
+poetry shell
+```
+
+Should now be able to run the command `streeteasy-grep --help` in a console.
+
+### Usage
+
+```
+usage: streeteasy-grep [-h] [--location LOCATION] [--price-lower-bound PRICE_LOWER_BOUND] [--price-upper-bound PRICE_UPPER_BOUND]
+                       [--num-bedrooms NUM_BEDROOMS] [--has-fee] [--check-diff]
+
+Parse streeteasy rental results for a given query with given parameters.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --location LOCATION, -l LOCATION
+                        Location to search for. (default: ues)
+  --price-lower-bound PRICE_LOWER_BOUND, -pl PRICE_LOWER_BOUND
+                        Lower bound of rental price (default: 0)
+  --price-upper-bound PRICE_UPPER_BOUND, -pu PRICE_UPPER_BOUND
+                        Upper bound of rental price (default: 3000)
+  --num-bedrooms NUM_BEDROOMS, -nb NUM_BEDROOMS
+                        Number of bedrooms (default: 1)
+  --has-fee, -hf        Include apartments that have a signing fee. (default: False)
+  --check-diff, -cd     Check diff between queries if the same query file has been created before (default: False)
+  ```
+
+### Sample Usage
+
+By default running `streeteasy-grep` will generate the query parameters:
+
+```
+ues/price:0-3000|beds:1|no_fee
+```
+
+Translating to quering for a 1 bedroom apartment on the Upper East Side, between $0-$3000/month and only including listing which are `no fee`.
+
+The results will be stored in a file called `results-{hash}.json` where `hash` is the hashed query string.
+
+It'll contain a dictionary with the `links` keys, pointing to a object containing `address` and `price`:
+
+```
+{"https://streeteasy.com/building/1600-3-avenue-new_york/5c": {"Address": "174 East 90th Street #5C", "Price": "$2,100"}
+```
+
+### Checking diff
+
+The tool is useful in checking for new listings using a previous innvocation with **the same query**. Simply specifing:
+
+```
+streeteasy-grep --check-diff
+```
+
+Will diff the new query contents with an existing json file (in the directory the tool is invoked) and print to `stdout`.
--- streeteasy_grep/parser.py
+++ streeteasy_grep/parser.py
@@ -2,26 +2,28 @@
 
 import sys
 import json
+import pprint
+import hashlib
+import argparse
 import time
-from selenium import webdriver
+import os
 
+import selenium
+from selenium import webdriver
 from streeteasy_grep import config as cfg
 
 
 def construct_url(args=None):
     """ Construct street easy url given args """
 
-    # TODO: Use args make it dynamic
-    lower_bound = 0
-    upper_bound = 3000
-    # TODO: Support multiple locations (have to manually find each keyword)
-    location = "ues"
-    beds = 1
-    no_fee = True
+    lower_bound = args.price_lower_bound
+    upper_bound = args.price_upper_bound
+    location = args.location
+    beds = args.num_bedrooms
 
     url = f"{cfg.STREETEASY_SITE}/for-rent/{location}/price:{lower_bound}-{upper_bound}|beds:{beds}"
 
-    if no_fee:
+    if not args.has_fee:
         url += f"|no_fee"
 
     return url
@@ -29,50 +31,127 @@ def construct_url(args=None):
 
 def sanitize_link(link):
     """ Santiize link from any non-standard features. """
-
     if not link:
         return ""
+
     # Remove query paramaters
     link = link.split("?")[0]
-
     return link
 
 
-def main():
+def check_diff(object, file):
+    """ Print diff if file already exists. """
+    if not os.path.exists(file):
+        return
+
+    with open(file, "r") as f:
+        prev_obj = json.loads(f.read())
+
+    # Print the diff between the two
+    pprint.pprint({k: prev_obj[k] for k in set(prev_obj) - set(object)})
+
+
+def write_to_json(object, file, check_diff=False):
+    """ Write json object to a file, if check_diff is True, will return diff of previous file if it exists. """
+    if check_diff:
+        check_diff(object, file)
+
+    with open(file, "w+") as f:
+        json.dump(object, f)
+
+
+def parse_args(args):
+    """ Parse and store args """
+    parser = argparse.ArgumentParser(
+        description="Parse streeteasy rental results for a given query with given parameters.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        "--location", "-l", default="ues", type=str, help="Location to search for."
+    )
+    parser.add_argument(
+        "--price-lower-bound",
+        "-pl",
+        default=0,
+        type=int,
+        help="Lower bound of rental price",
+    )
+    parser.add_argument(
+        "--price-upper-bound",
+        "-pu",
+        default=3000,
+        type=int,
+        help="Upper bound of rental price",
+    )
+    parser.add_argument("--num-bedrooms", "-nb", default=1, help="Number of bedrooms")
+    parser.add_argument(
+        "--has-fee",
+        "-hf",
+        action="store_true",
+        help="Include apartments that have a signing fee.",
+    )
+    parser.add_argument(
+        "--check-diff",
+        "-cd",
+        action="store_true",
+        help="Check diff between queries if the same query file has been created before",
+    )
+
+    return parser.parse_args(args)
+
+
+def main(args=None):
     """ Setup and parse results"""
     # Requires gecko driver to be installed.
     # https://github.com/mozilla/geckodriver/releases
     opts = webdriver.FirefoxOptions()
     opts.headless = True
     driver = webdriver.Firefox(options=opts)
-    url = construct_url()
-    print(f"Parsing url: [{url}]")
-    driver.get(url)
+    args = parse_args(args)
+    url = construct_url(args)
 
-    # Start parsing out the result content
-    # This is extremely fragile and required looking through the HTML for the div ids
-    search_results = driver.find_element_by_class_name("searchCardList")
-    result_list = search_results.find_elements_by_tag_name("li")
-    results_dictionary = {}
-    for result in result_list:
-        # Get the listing link
-        a_el = result.find_elements_by_class_name("listingCard-globalLink")
-        link = sanitize_link(a_el[0].get_attribute("href"))
-        # Get text info
-        # Have to use xpath to get span elements
-        text_el = result.find_elements_by_xpath('.//span[@class = "u-displayNone"]')
-        print(text_el)
-        text = text_el[0].text
-        # Get price
-        price_el = result.find_elements_by_class_name("price")
-        price = price_el[0].text
+    try:
+        # Iterate through all pages of results, and exception will be thrown to end iteration.
+        page = 1
+        results_dictionary = {}
+        while True:
+            print(f"Parsing url: [{url}?page={page}]")
+            driver.get(f"{url}/?page={page}")
+            # Start parsing out the result content
+            # This is extremely fragile and required looking through the HTML for the class/div ids
+            search_results = driver.find_element_by_class_name("searchCardList")
+            result_list = search_results.find_elements_by_tag_name("li")
+            for result in result_list:
+                # Get the listing link
+                a_el = result.find_elements_by_class_name("listingCard-globalLink")
+                link = sanitize_link(a_el[0].get_attribute("href"))
+                # Get the unit address
+                address_el = result.find_elements_by_class_name(
+                    "listingCard-addressLabel"
+                )
+                address = address_el[0].text
+                # Get price
+                price_el = result.find_elements_by_class_name("price")
+                price = price_el[0].text
 
-        results_dictionary[link] = {"Description": text, "Price": price}
+                results_dictionary[link] = {"Address": address, "Price": price}
+            page += 1
 
-    print(results_dictionary)
+    except selenium.common.exceptions.WebDriverException as e:
+        print(f"{str(e)}")
+
+    # Write to file using url hash as identifier
+    # Also store the query in the output dictionary
+    dynamic_url = url.split("for-rent/")
+    query_content = dynamic_url[1]
+    hashed_query = hashlib.sha1(query_content.encode()).hexdigest()
+    results_dictionary["query"] = query_content
+    write_to_json(results_dictionary, f"results-{hashed_query}.json", args.check_diff)
 
     driver.close()
+    driver.quit()
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(main(sys.argv[1:]))
